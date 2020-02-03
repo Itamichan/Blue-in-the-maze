0b98dcfe7b78ff7d763d82ab61a0bc213f80959a
Itamichan <cristinagarbuz@gmail.com>
2020-02-03 15:24:23 +0100

    setting the player's start position

--- src/Components/MazeContainer.js
+++ src/Components/MazeContainer.js
@@ -5,6 +5,9 @@ const MazeContainer = (props) => {
 
     const {mazeGrid, currentLocation, up, down, right, left, finish} = useMaze();
 
+    // 3 stands for the player location
+    mazeGrid[currentLocation[0]][currentLocation[1]] = 3;
+
     return (
         <div>
 
