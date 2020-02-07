73466106dfbcf5ac5357295efd2c62e4fbb83adb
Itamichan <cristinagarbuz@gmail.com>
2020-02-07 11:34:09 +0100

    final style for ControlButtons

--- src/Components/Buttons/ControlButtons/ControlButtons.js
+++ src/Components/Buttons/ControlButtons/ControlButtons.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Button, Col, Container, Row} from "reactstrap";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import './ControlButtons.scss';
 
 import PropTypes from 'prop-types';
 
