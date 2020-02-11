2719fa51c9e65941822fa3a83b5d3e1dc4a290e9
Itamichan <cristinagarbuz@gmail.com>
2020-02-11 17:46:14 +0100

    updated the style for the footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,12 +6,14 @@ import './Footer.scss';
 const Footer = (props) => {
     return (
         <footer>
-            <ul>
-                <li><a href="https://github.com/Itamichan" rel='noreferrer noopener' target="_blank"> <FontAwesomeIcon icon={['fab', 'github']} /></a></li>
-                <li><a href="mailto:garbuz@gmail.com"><FontAwesomeIcon icon='envelope' /></a></li>
-                <li><a href="https://facebook.com" rel='noreferrer noopener' target="_blank"> <FontAwesomeIcon icon={['fab', 'facebook']} /></a></li>
+            <div>
+                <ul>
+                    <li><a href="https://github.com/Itamichan" rel='noreferrer noopener' target="_blank"> <FontAwesomeIcon icon={['fab', 'github']} /></a></li>
+                    <li><a href="mailto:garbuz@gmail.com"><FontAwesomeIcon icon='envelope' /></a></li>
+                    <li><a href="https://www.facebook.com/cristina.garbuz" rel='noreferrer noopener' target="_blank"> <FontAwesomeIcon icon={['fab', 'facebook']} /></a></li>
 
-            </ul>
+                </ul>
+            </div>
         </footer>
     )
 };
