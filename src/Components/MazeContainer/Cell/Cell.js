c1d908eedf9f122cfc161d49d6576c7c710a10ed
Itamichan <cristinagarbuz@gmail.com>
2020-02-15 17:31:44 +0100

    rendering the correct bg for the cells

--- src/Components/MazeContainer/Cell/Cell.js
+++ src/Components/MazeContainer/Cell/Cell.js
@@ -1,25 +1,47 @@
 import React from 'react';
 import './Cell.scss';
+import {KEYS} from "../LevelSelection/levels";
+
+//todo change the img names to be relevant
+
+const bgPlayer = '/resources/p1_walk01.png';
+
+
+const BACKGROUND_MAPPING = {
+    0: '/resources/brickWall.png',
+    1: '/resources/castleCenter.png',
+    2: '/resources/grassCenter.png',
+    3: '/resources/door_closedMid.png',
+    4: '/resources/keyGreen.png',
+    5: '/resources/keyYellow.png',
+    6: '/resources/keyRed.png',
+    7: '/resources/keyBlue.png',
+};
 
 //component which represents every div in the mazeGrid
-const Cell = ({cellValue, isPlayerHere, userBag}) => {
+const Cell = React.memo(({cellValue, isPlayerHere, userBag}) => {
 
+    let cellBackgrounds = [];
 
-    switch (cellValue) {
-        case 0:
-            return <div className={'empty-field'}/>;
-        case 1:
-            return <div className={'wall-field'}/>;
-        case 2:
-            return <div className={isPlayerHere ? 'path-field-player' : 'path-field'}/>;
-        case 3:
-            return <div className={isPlayerHere ? 'finish-field-player' : 'finish-field'}/>;
-        case 4:
-            return <div className={isPlayerHere ? 'path-field-player' : 'path-field-key'}/>;
+    const buildBackground = (backgroundPaths) => {
+        let styledString = backgroundPaths.map(e => `url(${process.env.PUBLIC_URL + e})`);
+        return styledString.join(',')
+    };
 
-        default:
-            return <div className={'red'}/>
+    if (isPlayerHere) {
+        cellBackgrounds.push(bgPlayer)
     }
-};
+
+    if (!userBag.includes(cellValue)) {
+        cellBackgrounds.push(BACKGROUND_MAPPING[cellValue]);
+    }
+
+    if (KEYS.includes(cellValue)) {
+        cellBackgrounds.push(BACKGROUND_MAPPING[2])
+    }
+
+
+    return <div style={{background: buildBackground(cellBackgrounds)}}/>
+});
 
 export default Cell;
