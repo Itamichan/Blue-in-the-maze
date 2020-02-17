95d8f2701d6b90733166cdc884302b500dce946c
Itamichan <cristinagarbuz@gmail.com>
2020-02-17 16:34:45 +0100

    added comments to useMaze Hook

--- src/Components/MazeContainer/useMaze.js
+++ src/Components/MazeContainer/useMaze.js
@@ -3,21 +3,25 @@ import {LEVELS, KEYS} from "./LevelSelection/levels";
 
 const useMaze = (mazeLevel, onPlayerLose) => {
 
-    // data structure of the MazeContainer
-    // numbers meaning: 0 - empty; 1 - wall; 2 - path; 3 - finish
     const mazeGrid = LEVELS[mazeLevel].levelGrid;
     const timeLeft = LEVELS[mazeLevel].timeLeft;
+
+    //identifies if the timer should count up or down
     const countDirection = timeLeft === undefined ? 'up' : 'down';
 
 
-    // setting the currentLocation variable
+    // keeps track of user's location
     const [currentLocation, setCurrentLocation] = useState(LEVELS[mazeLevel].levelStart);
     const [startTime, setStartTime] = useState(new Date());
+
+    //keeps track of collected keys
     const [userBag, setUserBag] = useState([]);
+
+    //sets the start time for the timer
     const [screenTime, setScreenTime] = useState(countDirection === 'up' ? 0 : timeLeft);
     const [playerLost, setPlayerLost] = useState(false);
 
-
+    //updates the timer in dependence of the countDirection
     let updateScreenTime = () => {
         let timeDifference = (new Date() - startTime) / 1000;
         let newScreenTime = countDirection === 'up' ? timeDifference : timeLeft - timeDifference;
@@ -28,7 +32,7 @@ const useMaze = (mazeLevel, onPlayerLose) => {
         setScreenTime(Math.floor(newScreenTime))
     };
 
-
+    //runs the updateScreenTime function every 50ms
     useEffect(() => {
 
             let intervalId = setInterval(() => {
@@ -46,8 +50,9 @@ const useMaze = (mazeLevel, onPlayerLose) => {
     let finish = false;
     if (mazeGrid[currentLocation[0]][currentLocation[1]] === 3) {
         finish = true;
+        //accesses the local storage to see if a level have been finished if not the level is added
+        //to the local storage once
         let completedLevels = localStorage.getItem('completedLevels');
-        // let levelsArray = completedLevels;
         if (completedLevels === null) {
             completedLevels = [];
         } else completedLevels = completedLevels.split(",");
@@ -64,16 +69,19 @@ const useMaze = (mazeLevel, onPlayerLose) => {
 
         let newLocation = [Y + deltaY, X + deltaX];
         let gridValue = mazeGrid[newLocation[0]][newLocation[1]];
-        // doesn't allow to walk into a wall
+        //
         if (canMove(gridValue)) {
             setCurrentLocation(newLocation);
+            //adds the key to the userBag if the player doesn't have it
             if (KEYS.includes(gridValue) && !userBag.includes(gridValue)) {
                 setUserBag([...userBag, gridValue])
             }
         }
     };
 
+    //sets when the player can walk and when not
     const canMove = (gridValue) => {
+        //checks if the player has the key to pass the door
         let haveKey = userBag.some((key) => {
             return key * 11 === gridValue
         });
@@ -81,13 +89,14 @@ const useMaze = (mazeLevel, onPlayerLose) => {
         if (gridValue === 1 || gridValue === 0) {
             return false
         }
-        //allow to step on the path/ finish or on the keys
+        //allows to step on the path/ finish/ keys
         if (gridValue === 2 || gridValue === 3 || KEYS.includes(gridValue)) {
             return true
         }
         return haveKey
     };
 
+    //all 4 functions bellow specifies in which direction the player will move
     const up = () => {
         if (currentLocation[0] > 0) {
             move(-1, 0)
@@ -117,13 +126,13 @@ const useMaze = (mazeLevel, onPlayerLose) => {
         mazeGrid,
         currentLocation,
         finish,
+        screenTime,
+        playerLost,
+        userBag,
         up,
         down,
         right,
         left,
-        screenTime,
-        playerLost,
-        userBag
     }
 };
 
